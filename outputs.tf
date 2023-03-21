0bed2ff669e51fb14e47848273474d37c1ba9d73
Yeghishe <marsel8937@gmail.com>
2023-03-21 08:01:57 +0000

    changinx exo-lb to load-balancer in output values

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "load-balancer-id"  {
- value = aws_elb.exo-lb.id
+ value = aws_elb.load-balancer.id
 }
 
 output "load-balancer-dns-name"  {
- value = aws_elb.exo-lb.dns_name
+ value = aws_elb.load-balancer.dns_name
 }
